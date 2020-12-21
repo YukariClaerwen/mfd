b1c1a7766eaa901f0476b08a15bfef79f5902a50
hiennguyen1998 <hiennguyen30081998@gmail.com>
2020-12-21 17:28:01 +0700

    update sql

--- data/Dump20201220.sql
+++ data/Dump20201220.sql
@@ -43,4 +43,93 @@ begin
 select * from temp_bvTheoTuKhoa;
 end $$
 delimiter ;
- 
+
+/* Thêm bình luận */
+ delimiter $$
+create procedure Sp_Thembinhluan (in noidung text, in idbv int, in tentk varchar(255))
+begin
+	insert into tbl_binhluan(Noidung,Ngaybinhluan,Idbaiviet,Tentaikhoan) values (noidung,current_timestamp(),idbv,tentk);
+end $$
+delimiter ;
+
+/* Xóa bình luận */
+  delimiter $$
+create procedure Sp_Xoabinhluan (in idbl int)
+begin
+	delete from tbl_binhluan where Idbinhluan=idbl;
+end $$
+delimiter ;
+drop trigger tg_after_insert_baiviet;
+/* Đăng bài viết */
+  delimiter $$
+create procedure Sp_Thembaiviet (in content text,in storename varchar(255) ,in score int, in address varchar(255), in location varchar(255)
+,in timeopen varchar(255),in price varchar(255), in statuss varchar(255), in title text, in image varchar(255), in username varchar(255))
+begin
+	insert into tbl_baiviet(Noidung,Soluotxem,Tenquan,Diemdanhgia,Diachiquan,Diadiem,Thoigianmocua,Giaban,Trangthaibaiviet,Dachinhsua,Ngaydangbaiviet,Tieudebaiviet,soluotthich,hinhmacdinh,Tentaikhoan)
+    values(content,0,storename,score,address,location,timeopen,price,statuss,'Chưa',current_timestamp(),title,0,image,username);
+end $$
+delimiter ;
+
+/* Thêm hình bài viết */
+  delimiter $$
+create procedure Sp_Themhinhbaiviet (in url varchar(255),in id int)
+begin
+	insert into tbl_hinhanh (Duongdan,Idbaiviet) values (url,id);
+end $$
+delimiter ;
+
+/* Thêm hashtag của bài viết*/
+drop procedure Sp_Themhashtag;
+  delimiter $$
+create procedure Sp_Themhashtag (in idbv int, in tenhtg varchar(255),in tag varchar(255))
+begin
+	declare idt int;
+	if exists(select Tentag from tbl_hashtag where Tentag=tag) then
+		select Idtag from tbl_hashtag where Tentag=tag into idt;
+		insert into tbl_tagbaiviet (Idtag,Idbaiviet) values (idt,idbv);
+	else
+		insert into tbl_hashtag(Tentag,tenhashtag) values (tag,tenhtg);
+        select max(Idtag) from tbl_hashtag into idt ;
+        insert into tbl_tagbaiviet (Idtag,Idbaiviet) values (idt,idbv);
+	end if;
+end $$
+delimiter ;
+
+/* Sửa đăng ký */
+drop procedure if exists Sp_Dangky;
+delimiter $$
+create procedure Sp_Dangky (in tentk varchar(255), in mail varchar(255),in mk1 varchar(255),in mk2 varchar(255),out mess int)
+begin
+	if(exists(select Tentaikhoan from tbl_taikhoan where Tentaikhoan=tentk))then
+		select 1 into mess;
+	elseif(exists(select Email from tbl_taikhoan where Email=mail))then
+		select 2 into mess;
+	elseif (mk1 <> mk2 )then
+		select 3 into mess;
+	else
+		insert into tbl_taikhoan(Email,Tentaikhoan,Matkhau,Tenhienthi,Ngaytaotaikhoan,Idtrangthai,Idquyen)
+        values (mail,tentk,mk1,tentk,current_timestamp(),2,0);
+        select 4 into mess;
+	end if;
+    select mess;
+end $$
+delimiter ;
+
+/*Chỉnh sửa thông tin */
+/* 1 Eamil đã tồn tại, 2 update thành công*/
+drop procedure if exists Sp_Chinhsuathongtin;
+delimiter $$
+create procedure Sp_Chinhsuathongtin (in mail varchar(255), in tentk varchar(255),in nghenghiep varchar(255),in gt varchar(255),in ns date,out mess int)
+beginn
+	if(exists(select Email from tbl_taikhoan where Email=mail))then
+		select 1 into mess;
+	else
+		update tbl_taikhoan set Email=mail,Congviec=nghenghiep,Gioitinh=gt, ngaysinh=ns where Tentaikhoan= tentk ;
+        select 2 into mess;
+	end if;
+    select mess;
+end $$
+delimiter ;
+
+call Sp_Chinhsuathongtin('hien@gmail.com','hien','Nong dan','Nam','2000-02-19',@mess);
+call Sp_Chinhsuathongtin('hien567@gmail.com','hien','Nong dan','Nam','2000-02-19',@mess)
